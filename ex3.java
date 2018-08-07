5c02eb23a0369aee3016ebfef73ad2f4e900ebaa
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-08-07 10:13:44 +0530

    Create ex3.java

---
+++ ex3.java
@@ -0,0 +1,29 @@
+import java.util.*;
+import java.lang.*;
+
+class ex3
+{  
+  
+  
+	  
+	 int length;  
+	 int width;  
+	  
+	 void insert(int l,int w){  
+	  length=l;  
+	  width=w;  
+	 }  
+	  
+	 void calculateArea(){System.out.println(length*width);}  
+	  
+	 public static void main(String args[]){  
+	  ex3 r1=new ex3();  
+    ex3 r2=new ex3();  
+	  
+	  r1.insert(11,5);  
+	  r2.insert(3,15);  
+	  
+	  r1.calculateArea();  
+	  r2.calculateArea();  
+	}  
+	}  
