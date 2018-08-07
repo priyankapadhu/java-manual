5536b6788b004da2803c68dc54b1481f0e3edff8
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-08-07 10:34:47 +0530

    Create ex6.java

---
+++ ex6.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+
+class ex5
+{  
+  
+	  void sum(int a,int b){System.out.println(a+b);}  
+	  void sum(int a,int b,int c){System.out.println(a+b+c);}  
+	  
+	  public static void main(String args[]){  
+ ex6 obj=new Rextester();  
+ obj.sum(10,10,10);  
+	  obj.sum(20,20);  
+	  
+	  }  
+	}  
