368c2c0cd554b222be3de3505c541ef192d6200e
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-08-07 11:01:26 +0530

    Create ex9.java

---
+++ ex9.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.lang.*;
+
+
+class Rextester
+{  
+ 	  
+	  void sum(int a,long b){System.out.println(a+b);}  
+	  void sum(int a,int b,int c){System.out.println(a+b+c);}  
+	  
+	  public static void main(String args[]){  
+	  Rextester obj=new Rextester();  
+	  obj.sum(20,20);
+	  obj.sum(20,20,20);  
+	  
+	  }  
+	}  
