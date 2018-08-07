e5b4157daffcca8d7bbb4426b51cec5a01361255
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-08-07 11:07:18 +0530

    Create ex11.java

---
+++ ex11.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+
+
+class Rextester
+{  
+ 	  	 
+            Rextester(){System.out.println("Bike is created");}  
+	public static void main(String args[]){  
+	Rextester b=new Rextester();  
+	
+	}
+}
+
