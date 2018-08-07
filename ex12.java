4397144083fe37534fe95ab396bfdeedf3eab786
priyankapathu <40590328+priyankapathu@users.noreply.github.com>
2018-08-07 11:14:33 +0530

    Create ex12.java

---
+++ ex12.java
@@ -0,0 +1,19 @@
+
+import java.util.*;
+import java.lang.*;
+
+
+class Rextester
+{  
+int id;  
+	String name;  
+	  
+	void display(){System.out.println(id+" "+name);}  
+	  
+	public static void main(String args[]){  
+	Rextester s1=new Rextester();  
+	Rextester s2=new Rextester();  
+	s1.display();  
+	s2.display();  
+	}  } 
+	
